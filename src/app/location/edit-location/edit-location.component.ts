af3211ad3bde1cf0d9fe73f240bf4f08d6db7d57
Danodya Weerasinghe <danodya@interns.fortunaglobal.com>
2018-10-16 11:04:38 +0530

    multiple circles

--- src/app/location/edit-location/edit-location.component.ts
+++ src/app/location/edit-location/edit-location.component.ts
@@ -15,25 +15,11 @@ export class EditLocationComponent implements OnInit {
   radius: number;
 
  // locationChosen = false;
- markers: Marker[] = [
+ circles: Circle[] = [
   {
-    lat: 51.673858,
-    lng: 7.815982,
+    lat: 6.8602,
+    lng: 80.0535,
     label: 'A',
-    radius: 9000,
-    draggable: true
-  },
-  {
-    lat: 51.373858,
-    lng: 7.215982,
-    label: 'B',
-    radius: 9000,
-    draggable: true
-  },
-  {
-    lat: 51.723858,
-    lng: 7.895982,
-    label: 'C',
     radius: 9000,
     draggable: true
   }
@@ -49,7 +35,7 @@ export class EditLocationComponent implements OnInit {
   mapClicked($event: MouseEvent) {
     this.lat = $event.coords.lat;
     this.lng = $event.coords.lng;
-    this.markers.push({
+    this.circles.push({
             lat: $event.coords.lat,
             lng: $event.coords.lng,
             radius: 9000,
@@ -87,7 +73,7 @@ export class EditLocationComponent implements OnInit {
   // // }
 }
 // just an interface for type safety.
-interface Marker {
+interface Circle {
   lat: number;
   lng: number;
   label?: string;
